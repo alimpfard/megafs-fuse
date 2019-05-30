133e973b86d338f206f385e4b155f2ebb2072010
AnotherTest <devanothertest@gmail.com>
2019-05-30 04:55:54 +0430

    add example conf

---
+++ megafuse.conf.ex
@@ -0,0 +1,19 @@
+USERNAME = user.name@mail.com
+PASSWORD = pass.word
+
+# fs will be completely inacessible
+# after this quota runs out
+LOADBALANCE_QUOTA = 1024000
+
+# unspecified priority sets it to 1
+LOADBALANCE_PRIORITY = 2
+
+USERNAME = what@where.when
+PASSWORD = aaaaa
+LOADBALANCE_QUOTA = 1024000
+
+# mega.nz/#sdk
+APPKEY = ApPkEy
+
+MOUNTPOINT = /home/Test/Documents/megafs-fuse/mnt
+CACHEPATH = /tmp
